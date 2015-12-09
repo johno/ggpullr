cf4749dd955178a0e5912f7523088aa2f79fc48f
John Otander <johnotander@gmail.com>
2015-12-08 18:37:41 -0700

    Fix the git-current-branch call, again

--- ggpullr.sh
+++ ggpullr.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-git pull origin $(./node_modules/.bin/git-current-branch) --rebase
+git pull origin $(/usr/local/lib/node_modules/ggpullr/node_modules/.bin/git-current-branch) --rebase
