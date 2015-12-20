6718783364dd59f0f1f941a77e6ce752210591d3
John Otander <johnotander@gmail.com>
2015-12-19 18:16:12 -0700

    Ensure git-current-branch works for all npm versions

--- ggpullr.sh
+++ ggpullr.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env sh
 
-git pull origin $(/usr/local/lib/node_modules/ggpullr/node_modules/.bin/git-current-branch) --rebase
+currBranch=$(eval "$(node -e "var path = require('path');console.log(path.join(path.dirname(require('fs').realpathSync('$0')), 'node_modules/.bin/git-current-branch'))")")
+git pull origin $currBranch --rebase
